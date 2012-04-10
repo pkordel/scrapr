7c350e59350f680b7eeba7e22be804a6c8b82b26
Peter Kordel <pkordel@gmail.com>
2012-04-10 16:13:09 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,5 @@
+TODO
+====
+
+Using the home page at http://skoleverkstedet.deichman.no/ as starting point, 
+visit each letter of the index and extract all reviews on that page.
---
+++ scrapr.rb
@@ -0,0 +1,19 @@
+require 'mechanize'
+
+agent = Mechanize.new
+base_url = 'http://skoleverkstedet.deichman.no/'
+
+page  = agent.get(base_url + 'tema.htm')
+
+links = []
+links << page.search('//ul/li/a')
+links << page.search('//ul/li/strong/a')
+links = links.flatten.sort
+
+pages = []
+links.each { |l| pages << [l.text.gsub(/\r\n\s+/, ''), l[:href]] }
+
+page = agent.get(base_url + pages.first.last)
+links = page.search("//a[starts-with(@href, 'http://www.deich.folkebibl.no/cgi-bin/')]")
+
+puts links.size
